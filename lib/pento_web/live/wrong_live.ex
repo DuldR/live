b7eac71dc40fd9de566d066e47047b4be404474c
Garrett <gnjulaka@gmail.com>
2023-02-11 17:55:13 -0600

    Up to chap 2

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,85 @@
+defmodule PentoWeb.WrongLive do
+  alias PentoWeb.WrongLive
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+  alias PentoWeb.Router.Helpers, as: Routes
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     assign(socket,
+       answer: answer(),
+       time: time(),
+       score: 0,
+       message: "Make a guess:"
+     )}
+  end
+
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    if guess == socket.assigns.answer do
+      message = "Your guess: #{guess}. You're Right."
+      score = socket.assigns.score + 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          answer: answer(),
+          time: time(),
+          message: message,
+          score: score
+        )
+      }
+    else
+      message = "Your guess: #{guess}. You're Wrong."
+      score = socket.assigns.score - 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          time: time(),
+          message: message,
+          score: score
+        )
+      }
+    end
+  end
+
+  def answer do
+    Enum.random(1..10) |> Integer.to_string()
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h1> Answer: <%= @answer %></h1>
+    <%= if @score == 3 do%>
+      <%= live_patch to: Routes.live_path(@socket, PentoWeb.WrongLive) do %>
+        <button>
+          Restart
+        </button>
+
+      <% end %>
+    <% end %>
+
+    <h2>
+      <%= @message %>
+      It's <%= @time %>
+    </h2>
+
+    <h2>
+
+      <%= for n <- 1..10 do %>
+        <a href="#" phx-click="guess" phx-value-number={n} >
+          <%= n %>
+        </a>
+      <% end %>
+    </h2>
+
+
+    """
+  end
+end
